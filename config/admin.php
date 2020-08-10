2bc8aec07c9d3f2c8e2b4e462c67b824548c71a6
zhujianan <2494911084@qq.com>
2020-08-10 23:25:58 +0800

    引入常用扩展包

--- config/admin.php
+++ config/admin.php
@@ -22,7 +22,7 @@ return [
     | `img` tag, eg '<img src="http://logo-url" alt="Admin logo">'.
     |
     */
-    'logo' => '<img src="/vendors/dcat-admin/images/logo.png" width="35"> &nbsp;Jia Zi',
+    'logo' => '<img src="/static/img/zjntx.jpg" width="55"> &nbsp;Jia Zi',
 
     /*
     |--------------------------------------------------------------------------
@@ -34,7 +34,7 @@ return [
     | '<img src="http://logo-url" alt="Admin logo">'.
     |
     */
-    'logo-mini' => '<img src="/vendors/dcat-admin/images/logo.png">',
+    'logo-mini' => '<img src="/static/img/zjntx.jpg">',
 
     /*
      |--------------------------------------------------------------------------
